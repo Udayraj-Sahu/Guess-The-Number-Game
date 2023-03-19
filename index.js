ed8467b1b69987a0f9f3ddfd2267d7d13d67bc1f
Udayraj Sahu <96167329+Udayraj-Sahu@users.noreply.github.com>
2023-03-19 23:33:21 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,73 @@
+let computerGuess;
+let userGuess = [];
+let userGuessUpdate = document.getElementById("textoutput");
+let userNumberupdate = document.getElementById("inputBox");
+
+const init = () => {
+  computerGuess = Math.floor(Math.random() * 100);
+  document.getElementById("newGameButton").style.display = "none";
+  document.getElementById("gameArea").style.display = "none";
+};
+
+const startGame = () => {
+  document.getElementById("welcomeScreen").style.display = "none";
+  document.getElementById("gameArea").style.display = "block";
+};
+
+const newGameBegin = () => {
+  window.location.reload();
+};
+
+const startNewGame = () => {
+  document.getElementById("newGameButton").style.display = "inline";
+  userNumberupdate.setAttribute("disabled", true);
+};
+
+const compareGuess = () => {
+  const userNumber = Number(document.getElementById("inputBox").value);
+  userGuess = [...userGuess, userNumber];
+  document.getElementById("guesses").innerHTML = userGuess;
+  //start new game
+
+  // check the value low or high
+  if (userGuess.length < maxGuess) {
+    if (userNumber > compareGuess) {
+      userGuessUpdate.innerHTML = "Your Guess Is High 😯";
+      userNumberupdate.value = "";
+      startNewGame();
+    } else if (userNumber > compareGuess) {
+      userGuessUpdate.innerHTML = "Your Guess Is Low 😒";
+      userNumberupdate.value = "";
+      startNewGame();
+    } else {
+      userGuessUpdate.innerHTML = "Hurry! Right Guess😍";
+      userNumberupdate.value = "";
+      startNewGame();
+    }
+  } else {
+    if (userNumber > compareGuess) {
+      userGuessUpdate.innerHTML = `You Loose!! Correct Number Was ${compareGuess}`;
+      userNumberupdate.value = "";
+      startNewGame();
+    } else if (userNumber > compareGuess) {
+      userGuessUpdate.innerHTML = `You Loose!! Correct Number Was ${compareGuess}`;
+      userNumberupdate.value = "";
+      startNewGame();
+    } else {
+      userGuessUpdate.innerHTML = "Hurry! Right Guess😍";
+      userNumberupdate.value = "";
+      startNewGame();
+    }
+  }
+  document.getElementById("attempts").innerHTML = userGuess.length;
+};
+
+const easyMode = () => {
+  maxGuess = 10;
+  startGame();
+};
+
+const hardMode = () => {
+  maxGuess = 5;
+  startGame();
+};
